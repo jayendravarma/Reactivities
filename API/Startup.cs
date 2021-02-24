5b9f318fca0bf6325579aaf1434d4eb449615591
Jayendra P <jayendra.varma11@gmail.com>
2021-02-24 15:09:05 +0530

    second commit end of m-3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,11 @@ namespace API
                 
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -45,6 +50,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
